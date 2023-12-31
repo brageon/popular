2b6bef1ecb58a97ddc546496a97cf3b41b732b5a
brageon <153751096+brageon@users.noreply.github.com>
2023-12-31 23:59:29 +0100

    Update video.py

--- web/video.py
+++ web/video.py
@@ -5,3 +5,4 @@ file_name = 'strike.mp4'
 rsp = urllib2.urlopen(dwn_link)
 with open(file_name,'wb') as f:
     f.write(rsp.read())
+# melt a{1..9}.mp4 -consumer avformat:jen.mp4 acodec=libmp3lame vcodec=libx264
