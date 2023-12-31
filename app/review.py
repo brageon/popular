4d69e27a132ac5df87b7d21c38dec6213965558b
brageon <153751096+brageon@users.noreply.github.com>
2023-12-31 23:07:53 +0100

    Add files via upload

---
+++ app/review.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy 
+import csv
+# df = pd.read_fwf('dif.txt')
+# df.to_csv('dif.csv')
+df = pd.read_csv('dif.csv')
+ab = df.reviews.min('Tools')
+ac = df.reviews.max('Casual')
+ad = df.reviews.median('Arcade')
+print(ab,ac,ad)
+
+#tor = db.mean()
+#tor.to_csv('Ove.csv', mode='a')
+'''
+Subjects = []
+Scores = []
+  
+with open('../andro.csv', 'r') as csvfile:
+    lines = csv.reader(csvfile, delimiter = ',')
+    for row in lines:
+        Subjects.append(row[1])
+        Scores.append(int(row[2]))
+  
+plt.pie(Scores,labels = Subjects,autopct = '%.2f%%')
+plt.title('Marks of a Student', fontsize = 20)
+plt.show()
+'''
